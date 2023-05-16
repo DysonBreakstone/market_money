5b9fe25b46c5e31f770358ff0b83b2cb1595af27
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-16 16:36:13 -0600

    add uniquess validation for market_vendor

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,5 @@
 class MarketVendor < ApplicationRecord
+  validates :market_id, uniqueness: { scope: :vendor_id }
   validates_presence_of :market_id
   validates_presence_of :vendor_id
 
--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe MarketVendor, type: :model do
   describe "validations and relations" do
-    it {should validate_presence_of :market_id}
-    it {should validate_presence_of :vendor_id}
-
-    it {should belong_to :market}
-    it {should belong_to :vendor}
+    
+    it { should validate_presence_of :market_id }
+    it { should validate_presence_of :vendor_id }
+    
+    it { should belong_to :market }
+    it { should belong_to :vendor }
+    
+    it "validates vendor-market uniqueness" do
+      test_data
+      market_vendor = MarketVendor.new(vendor_id: @vendor_1.id, market_id: @market_1.id)
+      expect(market_vendor.save).to eq(false)
+    end
   end
 end
