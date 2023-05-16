f48077a4cab2bba81743b467fe4eb011203956dd
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-16 13:15:47 -0600

    happy and sad paths for vendor creation

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -9,6 +9,19 @@ class Api::V0::VendorsController < ApplicationController
     render json: VendorSerializer.new(Vendor.find(params[:id]))
   end
 
+  def create
+    @vendor = Vendor.new(name: params[:name],
+                         description: params[:description],
+                         contact_name: params[:contact_name],
+                         contact_phone: params[:contact_phone],
+                         credit_accepted: params[:credit_accepted])
+    if @vendor.save
+      render json: VendorSerializer.new(Vendor.find(@vendor.id))
+    else
+      render json: { errors: @vendor.errors.full_messages }, status: :bad_request
+    end
+  end
+
   def not_found(exception)
     render json: SearchFacade.handle_missing_error(exception), status: :not_found
   end
--- spec/requests/post_vendor_spec.rb
+++ spec/requests/post_vendor_spec.rb
@@ -9,13 +9,30 @@ RSpec.describe "Happy Path", type: :request do
     it "creates vendor with given all attributes" do
       expect(Vendor.count).to eq(10)
       post "/api/v0/vendors", params: { 
-                                        name: "Vendor 11"
-                                        description: "Description Eleven"
-                                        contact_name: "Contact Eleven"
-                                        contact_phone: "(111) 111-1111"
+                                        name: "Vendor 11",
+                                        description: "Description Eleven",
+                                        contact_name: "Contact Eleven",
+                                        contact_phone: "(111) 111-1111",
                                         credit_accepted: true
                                       }
       expect(Vendor.count).to eq(11)
+      expect(response).to have_http_status(:success)
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json).to have_key(:data)
+      expect(json[:data][:attributes][:name]).to eq("Vendor 11")
+    end
+  end
+
+  describe "Sad Path" do
+    it "gives back bad request" do
+      post "/api/v0/vendors", params: { 
+        name: "Vendor 11",
+        contact_name: "Contact Eleven",
+        contact_phone: "(111) 111-1111",
+      }
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(json[:errors]).to eq(["Description can't be blank", "Credit accepted is not included in the list"])
     end
   end
 end
