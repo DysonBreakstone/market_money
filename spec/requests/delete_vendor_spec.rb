9c96f776cafc24b0f1f58876004d71b4e671db9f
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-16 15:03:52 -0600

    happy path tests for delete vendor

---
+++ spec/requests/delete_vendor_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "delete vendor", type: :request do
+  describe "Happy Path" do
+    before do
+      test_data
+    end
+
+    it "deletes vendor" do
+      expect(Vendor.count).to eq(5)
+
+      delete "/api/v0/vendors/#{@vendor_4.id}"
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(Vendor.count).to eq(4)
+      expect(response).to have_http_status(204)
+      require 'pry'; binding.pry
+    end
+  end
+end
